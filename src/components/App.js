85bbb54ed433c2afe209d47e83976a2b75a87cfa
Julia Foresti <juliajforesti@gmail.com>
2021-03-28 18:56:49 -0300

    footer added

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import Map from './Map';
 
 import '../assets/css/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Footer from './Footer';
 
 function App() {
   return (
     <div>
     <Map />
+    <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Developed by Julia Foresti</p>
+    </footer>
+  );
+};
+
+export default Footer;
